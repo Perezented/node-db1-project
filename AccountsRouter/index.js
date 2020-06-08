d3adb6496853f6baa319733681436c1e874668fc
Michael Perez <mperez6473@gmail.com>
2020-06-08 15:59:06 -0500

    Have the accounts showing now.

--- AccountsRouter/index.js
+++ AccountsRouter/index.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const knex = require("../data/dbConfig");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    knex.select("*")
+        .from("Accounts")
+        .then((accounts) => {
+            console.log(accounts);
+            res.status(200).json({ accounts });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
 module.exports = router;
